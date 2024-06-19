afc122454d5788187e5292eb429a857681f01820
promahbubul <mahbublalam500@gmail.com>
2024-06-20 01:39:46 +0600

    Home Page [Category] component update

--- src/components/Home/Category/CategoryCard.jsx
+++ src/components/Home/Category/CategoryCard.jsx
@@ -2,9 +2,12 @@
 
 const CategoryCard = ({ img, title, total, className }) => {
   return (
-    <div className={`${className} rounded-xl w-full `}>
-      <div className="">
-        <img src={img} className="w-full" />
+    <div className={`rounded-xl w-full cursor-pointer group ${className}  `}>
+      <div className="overflow-hidden rounded-t-xl">
+        <img
+          src={img}
+          className="w-full rounded-t-xl group-hover:scale-105 transition-all duration-300"
+        />
       </div>
       <div className="flex flex-col gap-1 px-5 py-2 bg-silver3">
         <h4 className="text-[#03081F]  font-bold font-poppins text-lg">
