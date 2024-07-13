e9c41685d766455d5004359d355c71452d6ce548
promahbubul <mahbublalam500@gmail.com>
2024-07-13 20:46:58 +0600

    update hero section

--- src/components/RestaurentDetails/Banner/index.jsx
+++ src/components/RestaurentDetails/Banner/index.jsx
@@ -7,6 +7,7 @@ import IconButton from "../../ui/LogoButton";
 import { FaStar } from "react-icons/fa";
 import { FaStarHalfAlt } from "react-icons/fa";
 import { FaRegStar } from "react-icons/fa";
+import { TbClockHour9Filled } from "react-icons/tb";
 
 const Banner = () => {
   return (
@@ -23,9 +24,13 @@ const Banner = () => {
           <h1 className="text-6xl font-semibold mt-2 text-black">
             McDonald’s East London
           </h1>
-          <div className="flex flex-row gap-4 text-lg mt-8 font-medium">
+          <div className="flex mb-8  flex-row gap-4 text-lg mt-8 font-medium">
             <IconButton icon={GoChecklist} title={"Minimum Order: 12 GBP"} />
             <IconButton icon={IoBicycle} title={"Delivery in 20-25 Minutes"} />
+          </div>
+          <div className="bg-orange absolute flex flex-row items-center gap-3  -bottom-14 -left-14 py-4 px-16 rounded-r-xl text-white">
+            <TbClockHour9Filled className="text-2xl text-white" />
+            <p className="text-lg font-semibold">Open until 3:00 AM</p>
           </div>
         </div>
         {/* right */}
